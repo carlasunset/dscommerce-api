7890d61ebb2ba64acbf084535345a6bb42062464
Carla Lira <109699098+carlasunset@users.noreply.github.com>
2026-02-24 19:34:51 -0300

    initial setup

---
+++ src/test/java/com/carla/dscommerce/DsCommerceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.carla.dscommerce;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DsCommerceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
